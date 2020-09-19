1eceeab1b1c24bf850c28abb98c306c32360591d
Olivia Cubela <oliviacubela@gmail.com>
2020-09-19 04:36:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+# def call(env)
+#   req = Rack::Request.new
+#   reqp = Rack::Response.new(env)
+
+  # if req.path=="/"
+  # resp.write "Hello World!"
+  #
+  get "/" do
+    "Hello World!"
+  end
+
 
 
 end
